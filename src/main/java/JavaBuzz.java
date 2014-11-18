0c448b27454ac59ff7578fc07f2b4562c636d34c
Bernard Mordan <bernardmordan@gmail.com>
2014-11-18 11:35:11 +0000

    Setup with Gradle and Intellij

---
+++ src/main/java/JavaBuzz.java
@@ -0,0 +1,8 @@
+/**
+ * Created by bernie on 18/11/2014.
+ */
+public class JavaBuzz {
+    public boolean isDivisibleByThree(int number) {
+        return (number % 3) == 0;
+    }
+}
---
+++ src/test/java/JavaBuzzTests.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class JavaBuzzTests {
+
+    @Test
+    public void isDivisibleByThree() {
+        JavaBuzz javabuzz = new JavaBuzz();
+        assertTrue(javabuzz.isDivisibleByThree(3));
+    }
+    @Test
+    public void isNotDivisibleByThree() {
+        JavaBuzz javabuzz = new JavaBuzz();
+        assertFalse(javabuzz.isDivisibleByThree(1));
+    }
+
+
+}

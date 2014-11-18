8f254d7f1be8a9edffe6a3cc755beb4bc3047c8e
Bernard Mordan <bernardmordan@gmail.com>
2014-11-18 11:45:12 +0000

    Added gitignore

--- src/main/java/JavaBuzz.java
+++ src/main/java/JavaBuzz.java
@@ -5,4 +5,8 @@ public class JavaBuzz {
     public boolean isDivisibleByThree(int number) {
         return (number % 3) == 0;
     }
+
+    public boolean isDivisibleByFive(int number) {
+        return (number % 5) == 0;
+    }
 }
--- src/test/java/JavaBuzzTests.java
+++ src/test/java/JavaBuzzTests.java
@@ -6,14 +6,26 @@ import static org.junit.Assert.assertTrue;
 public class JavaBuzzTests {
 
     @Test
-    public void isDivisibleByThree() {
-        JavaBuzz javabuzz = new JavaBuzz();
-        assertTrue(javabuzz.isDivisibleByThree(3));
+    public void
+        isDivisibleByThree() {
+            JavaBuzz javabuzz = new JavaBuzz();
+            assertTrue(javabuzz.isDivisibleByThree(3));
     }
     @Test
-    public void isNotDivisibleByThree() {
-        JavaBuzz javabuzz = new JavaBuzz();
-        assertFalse(javabuzz.isDivisibleByThree(1));
+    public void
+        isNotDivisibleByThree() {
+            JavaBuzz javabuzz = new JavaBuzz();
+            assertFalse(javabuzz.isDivisibleByThree(1));
+    }
+    @Test
+    public void
+        isDivisibleByFive() {
+            JavaBuzz javabuzz = new JavaBuzz();
+            assertTrue(javabuzz.isDivisibleByFive(5));
+    }
+    @Test
+    public void
+        isNotDivisibleByFive() {
     }
 
 
